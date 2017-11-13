535845beb7086b370630067b788ede6d491fc4d5
xing liu <31227004+cathrinexing@users.noreply.github.com>
2017-11-12 19:52:01 -0500

    e

--- practice5/empty-example/sketch.js
+++ practice5/empty-example/sketch.js
@@ -1,76 +1,75 @@
 var interfaceItems = [];
 
 var brushSize = 10;
+afunction setup() {
+    createCanvas(1000, 600);
 
-function setup(){
-  createCanvas(1000,600);
+    interfaceItems.push(new interface(50, 100, 30, color(255, 51, 153)));
+    interfaceItems.push(new interface(100, 100, 30, color(102, 153, 255)));
+    interfaceItems.push(new interface(50, 150, 30, color(255, 204, 255)));
+    interfaceItems.push(new interface(100, 150, 30, color(153, 204, 255)));
+}
 
-  interfaceItems.push(new interface(50, 100, 30, color(255, 51, 153)));
-  interfaceItems.push(new interface(100, 100, 30, color(102, 153, 255)));
-  interfaceItems.push(new interface(50, 150, 30, color(255, 204, 255)));
-  interfaceItems.push(new interface(100, 150, 30, color(153, 204, 255)));
-  }
+function draw() {
 
-  function draw() {
+    fill(0);
+    ellipse(mouseX, mouseY, brushSize, brushSize);
 
-      fill(0);
-      ellipse(mouseX, mouseY, brushSize, brushSize);
-
-      interfaceItems[0].display();
-      interfaceItems[1].display();
-      interfaceItems[2].display();
-      interfaceItems[3].display();
-      interfaceItems[0].check();
-      interfaceItems[1].check();
-      interfaceItems[2].check();
-      interfaceItems[3].check();
+    interfaceItems[0].display();
+    interfaceItems[1].display();
+    interfaceItems[2].display();
+    interfaceItems[3].display();
+    interfaceItems[0].check();
+    interfaceItems[1].check();
+    interfaceItems[2].check();
+    interfaceItems[3].check();
 
 }
 
-function mousePressed(){
+function mousePressed() {
 
-  if(interfaceItems[0].check() == true){
-    brushSize++;
-  }
-
-  if (interfaceItems[1].check() == true) {
-      brushSize--;
-  }
-  if (interfaceItems[2].check() == true) {
-      brushSize=brushSize + 5;
-  }
-  if (interfaceItems[3].check() == true) {
-     brushSize= brushSize - 5;
-  }
-  }
-
-function interface(tempX, tempY, tempBoxSize, tempColor){
-  this.x = tempX;
-  this.y = tempY;
-  this.boxSize = tempBoxSize;
-  this.setFill = tempColor;
-  this.overlay = false;
-
-  this.display = function(){
-    noStroke();
-    fill(this.setFill);
-    ellipse(this.x, this.y, this.boxSize, this.boxSize);
-
-    if(this.overlay == true){
-      fill(0, 100);
-      ellipse(this.x, this.y, this.boxSize, this.boxSize);
+    if (interfaceItems[0].check() == true) {
+        brushSize++;
     }
 
-  }
-
-  this.check = function(){
-    if(mouseX > this.x && mouseX < (this.x + this.boxSize) && mouseY > this.y && mouseY < (this.y + this.boxSize) ){
-      this.overlay = true;
-      return true;
-    }else{
-      this.overlay = false;
-
-      return false;
+    if (interfaceItems[1].check() == true) {
+        brushSize--;
     }
-  }
+    if (interfaceItems[2].check() == true) {
+        brushSize = brushSize + 5;
+    }
+    if (interfaceItems[3].check() == true) {
+        brushSize = brushSize - 5;
+    }
+}
+
+function interface(tempX, tempY, tempBoxSize, tempColor) {
+    this.x = tempX;
+    this.y = tempY;
+    this.boxSize = tempBoxSize;
+    this.setFill = tempColor;
+    this.overlay = false;
+
+    this.display = function () {
+        noStroke();
+        fill(this.setFill);
+        ellipse(this.x, this.y, this.boxSize, this.boxSize);
+
+        if (this.overlay == true) {
+            fill(0, 100);
+            ellipse(this.x, this.y, this.boxSize, this.boxSize);
+        }
+
+    }
+
+    this.check = function () {
+        if (mouseX > this.x && mouseX < (this.x + this.boxSize) && mouseY > this.y && mouseY < (this.y + this.boxSize)) {
+            this.overlay = true;
+            return true;
+        } else {
+            this.overlay = false;
+
+            return false;
+        }
+    }
 }
