48e0066ad69eae2034e23a98caff2c716a5b9429
xing liu <31227004+cathrinexing@users.noreply.github.com>
2017-11-16 14:16:41 -0500

    4

--- practice5/README.md
+++ practice5/README.md
@@ -1,4 +1,7 @@
 
 # project 5
- I add 3 button to control the color of the brush below. on the second line, When i click the FIRST bottom button, the brush color becomes pink. When i click the SENCOND bottom button,the brush color becomes yellow.
-When i click the third bottom button,the brush color becomes grey.
+ I add 4 button to control the color of the brush below.
+ In the second line, When i click the first bottom button, the brush color becomes pink. 
+When i click the second bottom button,the brush color becomes yellow.
+When i click the third bottom button,the brush color becomes lavender.
+When i click the forth bottom button,the brush color becomes grey.
--- practice5/empty-example/sketch.js
+++ practice5/empty-example/sketch.js
@@ -1,37 +1,45 @@
 var interfaceItems = [];
 
 var brushSize = 10;
-var brushColor = (0,0,0);
+var brushColor = (0, 0, 0);
 
 
 function setup() {
     createCanvas(1000, 600);
 
     interfaceItems.push(new interface(50, 100, 30, color(255, 51, 153)));
+    text("Size+", 35, 130);
     interfaceItems.push(new interface(100, 100, 30, color(102, 153, 255)));
+    text("Size-", 85, 130);
     interfaceItems.push(new interface(50, 150, 30, color(255, 192, 203)));
     interfaceItems.push(new interface(100, 150, 30, color(255, 255, 0)));
     interfaceItems.push(new interface(150, 150, 30, color(230, 230, 250)));
-    interfaceItems.push(new interface(200, 150, 30, color(125,125,125)));
+    interfaceItems.push(new interface(200, 150, 30, color(125, 125, 125)));
+    text("pink", 40, 180);
+    text("yellow", 85, 180);
+    text("lavender", 130, 180);
+    text("gray", 190, 180);
+    interfaceItems.push(new interface(50, 200, 30, color(125, 125, 125)));
 }
 
 function draw() {
 
     fill(brushColor);
    ellipse(mouseX, mouseY, brushSize, brushSize);
-
     interfaceItems[0].display();
     interfaceItems[1].display();
     interfaceItems[2].display();
     interfaceItems[3].display();
-     interfaceItems[4].display();
+    interfaceItems[4].display();
     interfaceItems[5].display();
+    //interfaceItems[6].display();
     interfaceItems[0].check();
     interfaceItems[1].check();
     interfaceItems[2].check();
     interfaceItems[3].check();
-     interfaceItems[4].check();
- interfaceItems[5].check();
+    interfaceItems[4].check();
+    interfaceItems[5].check();
+    //interfaceItems[6].check();
 }
 
 function mousePressed() {
@@ -47,15 +55,16 @@ function mousePressed() {
         brushColor = "pink";
     }
     if (interfaceItems[3].check() == true) {
-        brushColor="yellow";
-        
-      } 
+        brushColor = "yellow";
+
+    }
     if (interfaceItems[4].check() == true) {
-        brushColor ="lavender";   
+        brushColor = "lavender";
     }
-      if (interfaceItems[5].check() == true) {
-        brushColor =(125,125,125);   
+    if (interfaceItems[5].check() == true) {
+        brushColor = (125, 125, 125);
     }
+  
 }
 
 function interface(tempX, tempY, tempBoxSize, tempColor) {
