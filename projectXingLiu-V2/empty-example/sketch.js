a8123ce8ece0f5ea028d41ba9cdf15d98951c1d7
xing liu <31227004+cathrinexing@users.noreply.github.com>
2017-12-07 13:47:18 -0500

    1

--- projectXingLiu-V2/empty-example/sketch.js
+++ projectXingLiu-V2/empty-example/sketch.js
@@ -123,9 +123,9 @@ function draw() {
         //console.log("We are totally in the zone!");
         image(shoes, 600, 350, 100, 100);
         setRate =2;
-         soundFile2.rate(2);
+         soundFile2.rate(1);
         if (!soundFile2.isPlaying()) {
-            soundFile2.setVolume(0.8);
+            soundFile2.setVolume(0.6);
             soundFile2.play();
         }
 
